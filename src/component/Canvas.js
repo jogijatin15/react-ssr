2ac25dbe0fd838bf589e67da6fbab516a37baef0
Jogi, Jatin <JATIN_JOGI@homedepot.com>
2020-06-08 01:34:25 -0400

    basic useEffect example

--- src/component/Canvas.js
+++ src/component/Canvas.js
@@ -6,87 +6,36 @@ import './App.css';
 const Canvas = () => {
   const [photos, setPhotos] = useState([]);
 
-  // state to capture each char user enters
-  const [searchText, setSearchText] = useState("");
-  
-  // we want to call query the api only when user submits and not on every 
-  // input char entered hence this state  holds the entire text, user has 
-  // entered, just before submitting form
-  // OR to avoid using 'query' state, 'axiosData' function call be called 
-  // directly on submit button click
-  const [query, setQuery] = useState("bentley");
-
-  const [loading, setLoading] = useState(false);
-
-  // to store any abrupt error while making api call
-  const [error, setError] = useState(null);
-
   // To run an effect and clean it up only once (on mount and unmount),
   // you can pass an empty array ([]) as a second argument. This tells React
   // that your effect doesn’t depend on any values from props or state, so it
   // never needs to re-run. This isn’t handled as a special case — it follows
   // directly from how the dependencies array always works
-
-  // By default, effects run after every completed render, but you can choose
-  // to fire them only when certain values have changed
-
-  // empty array [] tells react to run only when 
-  // compomnent mounts(componentDidMount) and on unmount (componentDidUpdate)
   useEffect(() => {
-    console.log("in use effect", query);
+    console.log("in use effect");
     axiosData();
-  }, [query]) 
+  }, [])
 
   const axiosData = async () => {
-    setLoading(true);
     const url = 'https://api.unsplash.com/search/photos';
-    const queryParam = query;
-    try {
-      const response = await axios.get(url, {
-        params: { query: queryParam },
-        headers: {
-          Authorization: 'Client-ID e787ed18a7837c4aff0a5d75c9495c8d6ed977e46048f2b9a5015132afbd4b11'
-        }
-      });
+    const response = await axios.get(url, {
+      params: { query: 'bmw' },
+      headers: {
+        Authorization: 'Client-ID e787ed18a7837c4aff0a5d75c9495c8d6ed977e46048f2b9a5015132afbd4b11'
+      }
+    });
 
-      const receivedPics = response.data.results;
-      console.log("DATA", receivedPics);
-      setPhotos(receivedPics);
-    } catch (e) {
-      setError(e);
-    }
-    setLoading(false);
+    const receivedPics = response.data.results;
+    console.log("DATA", receivedPics);
+    setPhotos(receivedPics);
   }
 
-  const handleSearchSubmit = () => {
-    setQuery(searchText);
-  }
-
-  const handleSearchInput = (e) => {
-    const searchText = e.target.value;
-    setSearchText(searchText);
-  }
-
-  // hasPics would be false on initial render only, after that it always has the 
-  // previous state, so loader wouldnt be shown on query change. To show loader 
-  // on every query update, use 'loading' state variable
   const hasPics = photos && photos.length;
-  const spinner = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif'; 
+  const spinner = 'https://media.giphy.com/media/jAYUbVXgESSti/giphy.gif';
 
   return (
     <>
       <div className="xp__container grid isBound">
-       
-        <div className="col__12-12">
-          <input id="search-text" placeholder="search" type="text" onChange={(e) => handleSearchInput(e)} />
-          {searchText &&
-              <button className="search-submit" type="button" onClick={handleSearchSubmit}>Submit</button> 
-          }
-          {!searchText &&
-            <button className="search-submit" type="button" disabled>Submit</button>
-          }   
-          <p>Showing search results for <strong>{query}</strong></p>
-        </div>
         <div className="col__12-12">
           <div className="xp__image">
             {hasPics &&
@@ -99,12 +48,9 @@ const Canvas = () => {
                   />
                 </div>
               )
-            )}
-            {!hasPics && !error &&
+              )}
+            {!hasPics &&
               <img src={spinner} />
-            }
-            {error &&
-              <p>An error has occured</p>
             }
           </div>
         </div>
